93351d74ff12f76febc015ad69b84b543bbe4903
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <antantos@iekdelta.gr>
2025-11-04 18:48:22 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:p-1 2xl:padding-r padding-b">Hero</section>
+    <section className="padding">PopularProducts</section>
+    <section className="padding">SuperQuality</section>
+    <section className="padding-x py-10">Services</section>
+    <section className="padding">SpecialOffer</section>
+    <section className="padding bg-pale-blue">CustomerReviews</section>
+    <section className="padding">Subscribe</section>
+    <section className="padding">Footer</section>
+  </main>
+);
+
+export default App;
