04fe065268ec3ae5fe387d5330f6826d8e44310a
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <ntantostasos@gmail.com>
2025-12-07 13:14:39 +0200

    fix(Subscribe): correct typo in div tag for Subscribe section

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -11,9 +11,9 @@ const Subscribe = () => {
       </h3>
       <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
         <input type="text" placeholder="subscribe@nike.com" className="input" />
-        <d iv className="flex max-sm:justify-end items-center max-sm:w-full">
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
           <Button label="Sign Up" fullWidth />
-        </d>
+        </div>
       </div>
     </section>
   );
